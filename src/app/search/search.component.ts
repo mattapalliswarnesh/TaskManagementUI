927d8132986947e571315963e69ce5aec57778e6
Hemaprashant <k.hemaprashant@gmail.com>
2020-05-23 16:42:35 +0530

    SamplePageCompleted

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  taskname: string;
+  type: string;
+  date1: number;
+  status: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {taskname: 'Prashant', type: 'Personal', date1:5/5/2020, status: 'New'},
+  {taskname: 'Swarnesh', type: 'Work', date1:4/5/2020, status: 'In-progress'},
+  {taskname: 'Ravi', type: 'Shopping', date1:1/5/2020, status: 'New'},
+  {taskname: 'Rohith', type: 'Others', date1:5/5/2020, status: 'New'},
+  {taskname: 'Swathi', type: 'Personal', date1:6/5/2020, status: 'Completed'},
+  {taskname: 'Vamsi', type: 'Personal', date1:8/5/2020, status: 'Completed'},
+  {taskname: 'Yuvarj', type: 'Work', date1:9/5/2020, status: 'In-Progress'},
+];
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-public courseName:string = "Angular First application";
+  displayedColumns: string[] = ['taskname', 'type', 'date1', 'status'];
+  dataSource = ELEMENT_DATA;
   constructor(
   ) { }
 
